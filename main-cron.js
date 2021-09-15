ec6c11293021cce03193cdad1dc310d1479da0fa
T.T.DAT <55250111+ttdat-thecodeguy@users.noreply.github.com>
2021-09-15 18:33:19 +0700

    fix Xử Lí kết thúc đấu giá

--- main-cron.js
+++ main-cron.js
@@ -1,5 +1,5 @@
 const CronJob = require("cron").CronJob;
-const mailer = require("../utils/mailer");
+const mailer = require("./utils/mailer");
 
 const sanPhamModel = require("./services/sanPhamModel");
 const dauGiaModel = require("./services/dauGiaModel");
@@ -39,46 +39,55 @@ var job = new CronJob(
           };
           //mailer to nguoiban + nguoithang
           const nguoi_thang = await taiKhoanModel.findById(cao_nhat.id_tra_cao_nhat)
-          
-          
-          mailer.send({
+                    
+          await mailer.send({
             from: "online.auction.11team@gmail.com",
             to: `${nguoi_thang.email}`,
             subject: "OnlineAuction11: Chúc Mừng Nhà Vô Địch.",
             html: `
-                  Xin chào ${nguoi_thang.ho_ten}, Chúc Mừng bạn đã chiến thắng trong cuộc đấu giá với sản phẩm ${products[i].ten_sp}.
-                  <br> 
-                      giá mua: ${don_hang.gia_mua},
-                      ngày đặt: ${don_hang.ngay_dat_hang},
-                      đơn hàng sẽ gởi đến bạn sau 48 tiếng
-                  <br>
+                  Xin chào ${nguoi_thang.ho_ten}, Chúc Mừng bạn đã chiến thắng trong cuộc đấu giá với sản phẩm ${products[i].ten}.
+                 
+                  <ul> 
+                      <li>giá mua: ${don_hang.gia_mua}</li>
+                      <li>ngày đặt: ${don_hang.ngay_dat_hang}</li>
+                      <li style="color:red">
+                          đơn hàng sẽ gởi đến bạn sau 48 tiếng
+                      </li>
+                  <ul>
                   (Đây là thư tự động vui lòng không phản hồi)
                   `,
           });
 
 
-          mailer.send({
+          await mailer.send({
             from: "online.auction.11team@gmail.com",
             to: `${email_nguoi_ban}`,
-            subject: `OnlineAuction11: Món Hàng ${products[i].ten_sp} của bạn đã kết thúc và có người thắng.`,
+            subject: `OnlineAuction11: Món Hàng ${products[i].ten} của bạn đã kết thúc và có người thắng.`,
             html: `
-                  Xin chào ${hoten_nguoi_ban}, Món hàng ${products[i].ten_sp} của bạn đã kết thúc.
+                  Xin chào ${hoten_nguoi_ban}, Món hàng ${products[i].ten} của bạn đã kết thúc.
                   <br> 
                       Vui lòng kiểm tra danh sách đơn hàng để biết thêm chi tiết
                   <br>
                   (Đây là thư tự động vui lòng không phản hồi)
                   `,
           });
+
+
           await donHangModel.themDonHang(don_hang);
+
+          /// khóa toàn bộ cuộc đấu giá trước lại
+          await dauGiaModel.updateStatus(products[i].id_sp, 1)
+          
+          /// khóa toàn bộ sản phẩm lại
           await sanPhamModel.updateStatus(products[i].id_sp, 1);
         } else {
           /// mailer to nguoiban
-          mailer.send({
+          await mailer.send({
             from: "online.auction.11team@gmail.com",
             to: `${email_nguoi_ban}`,
-            subject: `OnlineAuction11: Món Hàng ${products[i].ten_sp} của bạn đã kết thúc và không có ai đấu giá.`,
+            subject: `OnlineAuction11: Món Hàng ${products[i].ten} của bạn đã kết thúc và không có ai đấu giá.`,
             html: `
-                  Xin chào ${hoten_nguoi_ban}, Món hàng ${products[i].ten_sp} của bạn đã kết thúc.
+                  Xin chào ${hoten_nguoi_ban}, Món hàng ${products[i].ten} của bạn đã kết thúc.
                   <br> 
                       Chúng tôi rất tiếc khi không có người đấu giá, bạn có thể mở lại cuộc đấu giá trong trình quản lí trang web
                   <br>
