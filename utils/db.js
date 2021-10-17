66e991eca92bdd83fa0281139f069a722f330d5c
anhkhuong1306 <anhkhuong1306@gmail.com>
2021-10-17 10:51:26 +0700

    update db

--- utils/db.js
+++ utils/db.js
@@ -1,23 +1,23 @@
 module.exports = require('knex')({
-    client: 'mysql2',
-    connection: {
-      host : 'localhost',
-      port : 3306,
-      user : 'root',
-      password : '',
-      database : 'online-auction'
-    }
-  });
-
-
-
-  // module.exports = require('knex')({
   //   client: 'mysql2',
   //   connection: {
-  //     host : "us-cdbr-east-04.cleardb.com",
+  //     host : 'localhost',
   //     port : 3306,
-  //     user : "bcee0366b61953",
-  //     password : "291d44bb",
-  //     database : "heroku_1ab5fb504169dfe"
+  //     user : 'root',
+  //     password : '',
+  //     database : 'online-auction'
   //   }
   // });
+
+
+
+  module.exports = require('knex')({
+    client: 'mysql2',
+    connection: {
+      host : "us-cdbr-east-04.cleardb.com",
+      port : 3306,
+      user : "bcee0366b61953",
+      password : "291d44bb",
+      database : "heroku_1ab5fb504169dfe"
+    }
+  });
